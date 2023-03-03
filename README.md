ffc11a64a3b54cd4461fca62f4c456efa8482973
TBXark <tbxark@outlook.com>
2023-03-04 03:03:28 +0800

    - 支持同时绑定多个bot

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 |--|--|--|--|
 |API_KEY|OpenAI API Key|Environment Variables||
 |TELEGRAM_TOKEN|Telegram Bot Token|Environment Variables||
+|TELEGRAM_AVAILABLE_TOKENS|支持多个Telegram Bot Token|Environment Variables, 为可选项，只有一个bot的时候设置`TELEGRAM_TOKEN`即可|多个Token用`,`分隔|
 |WORKERS_DOMAIN|Workers域名|Environment Variables|不要加上https://|
 |CHAT_WHITE_LIST|聊天ID白名单|Environment Variables|多个ID用`,`分隔，不知道ID，和机器人聊一句就能返回|
 |I_AM_A_GENEROUS_PERSON|关闭白名单，允许所有人访问|Environment Variables|鉴于很多人不想设置白名单，或者不知道怎么获取ID，所以设置这个选项就能允许所有人访问， 值为`true`时生效|
@@ -34,48 +35,13 @@
 
 
 ## 部署流程
-
-1. 新建Telegram机器人, 获得Token
-    1. 打开Telegram并向 BotFather 发送 `/start` 命令
-    2. 发送 `/newbot` 命令，并给你的机器人起一个名字
-    3. 给你的机器人取一个唯一的用户名
-    4. BotFather 会生成一个 Token，复制下来保存好，这个 Token 是和你的机器人绑定的密钥，不要泄露给他人！
-    5. 稍后再Cloudflare Workers 的设置里 将这个 Token 填入 `TELEGRAM_TOKEN` 变量中
-    6. 在Telegram中找到BotFather, 发送`/setcommands`, 找到自己的机器人, 发送`new - 开始新对话`
-2. 注册OpenAI账号并创建API Key
-    1. 打开 [OpenAI](https://platform.openai.com) 注册账号
-    2. 点击右上角的头像，进入个人设置页面
-    3. 点击 API Keys，创建一个新的 API Key
-    4. 稍后再Cloudflare Workers 的设置里 将这个 Token 填入 `API_KEY` 变量中
-3. [部署Workers](https://developers.cloudflare.com/workers/)
-    1. 打开 [Cloudflare Workers](https://dash.cloudflare.com/?to=/:account/workers) 注册账号
-    2. 点击右上角的 Create a Worker
-    3. 将代码复制到编辑器中，保存
-4. [配置环境变量](https://developers.cloudflare.com/workers/platform/environment-variables/)(Settings-Variables-Environment Variables)，设置`API_KEY`，`TELEGRAM_TOKEN`,`WORKERS_DOMAIN`,`CHAT_WHITE_LIST`
-    1. 打开 [Cloudflare Workers](https://dash.cloudflare.com/?to=/:account/workers) 点击你的Workers，点击右上角的 Setting -> Variables
-    2. `API_KEY`：设置成 OpenAI API Key
-    3. `TELEGRAM_TOKEN`：设置成 Telegram Bot Token
-    4. `WORKERS_DOMAIN`：设置成你的Workers域名，例如`workers_name.username.workers.dev`
-    5. `CHAT_WHITE_LIST`：设置成允许访问的用户的ID，例如`123456789,987654321`，可以在和你的机器人聊天中使用`/new`指令获取
-5. [绑定KV数据](https://dash.cloudflare.com/:account/workers/kv/namespaces)(Settings-Variables-KV Namespace Bindings),名字设置为`DATABASE`
-    1. 点击右上角的 Create a Namespace, 名字随便取, 但是绑定的时候必须设定为DATABASE
-    2. 打开 [Cloudflare Workers](https://dash.cloudflare.com/?to=/:account/workers) 点击你的Workers
-    3. 点击右上角的 Setting -> Variables
-    4. 在 KV Namespace Bindings 中点击 Edit variables
-    5. 点击 Add variable
-    6. 设置名字为`DATABASE` 并选择刚刚创建的KV数据
-6. 运行 `https://workers_name.username.workers.dev/init` 绑定telegram
-8. 开始新对话，使用`/new`指令开始，之后每次都会将聊天上下文发送到ChatGPT
-9. 使用`SETENV KEY=VALUE`指令修改用户配置，例如`SETENV SYSTEM_INIT_MESSAGE=现在开始是喵娘，每句话已喵结尾
-
-
+详情见 [部署流程](./DEPLOY.md)
 
 ## 最佳实践
-
-新建多个机器人和workers, 每个机器人赋予不同的`SYSTEM_INIT_MESSAGE`。比如翻译专家，文案专家，代码专家。然后每次根据自己的需求和不同的机器人聊天，这样就不用经常切换配置属性。。
+新建多个机器人绑定到同一个workers，设置`TELEGRAM_AVAILABLE_TOKENS`,, 每个机器人赋予不同的`SYSTEM_INIT_MESSAGE`。比如翻译专家，文案专家，代码专家。然后每次根据自己的需求和不同的机器人聊天，这样就不用经常切换配置属性。。
 
 
 
 ## TODO LIST
 
-- [ ] 允许多个机器人同时绑定一个workers
+- [x] 允许多个机器人同时绑定一个workers
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // 推荐在Workers配置界面填写环境变量， 而不是直接修改这些变量
 // OpenAI API Key
-let API_KEY = 'PLEASE_REPLACE_WITH_YOUR_OPENAI_API_KEY';
+let API_KEY = null;
 // Telegram Bot Token
-let TELEGRAM_TOKEN = 'PLEASE_REPLACE_WITH_YOUR_TELEGRAM_BOT_TOKEN';
+let TELEGRAM_TOKEN = null;
+// Available Telegram Bot Tokens
+let TELEGRAM_AVAILABLE_TOKENS = null;
 // Workers Domain
-let WORKERS_DOMAIN = 'workers_name.username.workers.dev';
+let WORKERS_DOMAIN = null;
 // Disable white list
 let I_AM_A_GENEROUS_PERSON = false;
 // Chat White List
@@ -24,13 +26,13 @@ export default {
     try {
       initGlobalEnv(env);
       const {pathname} = new URL(request.url);
-      if (pathname.startsWith(`/telegram/${TELEGRAM_TOKEN}/webhook`)) {
-        return handleTelegramWebhook(request);
-      }
       if (pathname.startsWith(`/init`)) {
-        return bindTelegramWebHook();
+        return bindWebHookAction();
       }
-      return new Response('NotFound', {status: 404});
+      if (pathname.startsWith(`/telegram`) && pathname.endsWith(`/webhook`)) {
+        return telegramWebhookAction(request);
+      }
+      return new Response('NOTFOUND: ' + pathname, {status: 404});
     } catch (e) {
       console.error(e);
       return new Response('ERROR:' + e.message, {status: 200});
@@ -38,7 +40,7 @@ export default {
   },
 };
 
-// ///////// 初始化
+// /////// --  初始化
 
 function initGlobalEnv(env) {
   if (env.API_KEY) {
@@ -49,6 +51,9 @@ function initGlobalEnv(env) {
   }
   if (env.CHAT_WHITE_LIST) {
     CHAT_WHITE_LIST = env.CHAT_WHITE_LIST.split(',');
+  }
+  if (env.TELEGRAM_AVAILABLE_TOKENS) {
+    TELEGRAM_AVAILABLE_TOKENS = env.TELEGRAM_AVAILABLE_TOKENS.split(',');
   }
   if (env.WORKERS_DOMAIN) {
     WORKERS_DOMAIN = env.WORKERS_DOMAIN;
@@ -76,35 +81,78 @@ async function initUserConfig(id) {
   }
 }
 
-// ///////// Telegram
-
-async function bindTelegramWebHook() {
-  return await fetch(
-      `https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          url: `https://${WORKERS_DOMAIN}/telegram/${TELEGRAM_TOKEN}/webhook`,
-        }),
-      },
+async function initTelegramToken(token, request) {
+  if (TELEGRAM_TOKEN && TELEGRAM_TOKEN === token) {
+    return null;
+  }
+  if (TELEGRAM_AVAILABLE_TOKENS && Array.isArray(TELEGRAM_AVAILABLE_TOKENS)) {
+    if (TELEGRAM_AVAILABLE_TOKENS.includes(token)) {
+      TELEGRAM_TOKEN = token;
+      return null;
+    }
+  }
+  const {message} = await request.json();
+  return sendMessageToTelegram(
+      '你没有权限使用这个命令, 请请联系管理员添加你的Token到白名单',
+      TELEGRAM_TOKEN,
+      message.chat.id,
   );
 }
 
-async function handleTelegramWebhook(request) {
+// /////// --  Router
+
+async function bindWebHookAction() {
+  const result = [];
+  const tokenSet = new Set();
+  if (TELEGRAM_TOKEN) {
+    tokenSet.add(TELEGRAM_TOKEN);
+  }
+  if (TELEGRAM_AVAILABLE_TOKENS && Array.isArray(TELEGRAM_AVAILABLE_TOKENS)) {
+    TELEGRAM_AVAILABLE_TOKENS.forEach((token) => tokenSet.add(token));
+  }
+  for (const token of tokenSet) {
+    const resp = await fetch(
+        `https://api.telegram.org/bot${token}/setWebhook`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            url: `https://${WORKERS_DOMAIN}/telegram/${token}/webhook`,
+          }),
+        },
+    ).then((res) => res.json());
+    result.push(resp);
+  }
+  return new Response(JSON.stringify(result), {status: 200});
+}
+
+async function telegramWebhookAction(request) {
+  // token 预处理
+  const {pathname} = new URL(request.url);
+  const token = pathname.match(/^\/telegram\/(\d+:[A-Za-z0-9_-]{35})\/webhook/)[1];
+  const tokenError = await initTelegramToken(token, request);
+  if (tokenError) {
+    return tokenError;
+  }
+
+  // 加载用户配置
   const {message} = await request.json();
   if (message?.chat?.id) {
     await initUserConfig(message.chat.id);
   }
+
   const handlers = [
-    filterWhiteListHandler,
-    filterPureTextMessageHandler,
-    updateUserConfigWithMessage,
-    newChatContextHandler,
-    chatWithOpenAIHandler,
+    msgCheckEnvIsReady,
+    msgFilterWhiteList,
+    msgFilterUnknownTextMessage,
+    msgUpdateUserConfig,
+    msgCreateNewChatContext,
+    msgChatWithOpenAI,
   ];
+
+
   for (const handler of handlers) {
     try {
       const result = await handler(message);
@@ -118,10 +166,30 @@ async function handleTelegramWebhook(request) {
   return new Response('OK', {status: 200});
 }
 
-// ///////// Handler
+
+// /////// --  Handler
+
+// 检查环境变量是否设置
+async function msgCheckEnvIsReady(message) {
+  if (!API_KEY) {
+    return sendMessageToTelegram(
+        'OpenAI API Key 未设置',
+        TELEGRAM_TOKEN,
+        message.chat.id,
+    );
+  }
+  if (!DATABASE) {
+    return sendMessageToTelegram(
+        'DATABASE 未设置',
+        TELEGRAM_TOKEN,
+        message.chat.id,
+    );
+  }
+  return null;
+}
 
 // 过滤非白名单用户
-async function filterWhiteListHandler(message) {
+async function msgFilterWhiteList(message) {
   if (I_AM_A_GENEROUS_PERSON) {
     return null;
   }
@@ -136,7 +204,7 @@ async function filterWhiteListHandler(message) {
 }
 
 // 过滤非文本消息
-async function filterPureTextMessageHandler(message) {
+async function msgFilterUnknownTextMessage(message) {
   if (!message.text) {
     return sendMessageToTelegram(
         '暂不支持非文本格式消息',
@@ -148,7 +216,7 @@ async function filterPureTextMessageHandler(message) {
 }
 
 // 用户配置修改
-async function updateUserConfigWithMessage(message) {
+async function msgUpdateUserConfig(message) {
   if (!message.text.startsWith('SETENV')) {
     return null;
   }
@@ -185,7 +253,7 @@ async function updateUserConfigWithMessage(message) {
 }
 
 // 新的对话
-async function newChatContextHandler(message) {
+async function msgCreateNewChatContext(message) {
   if (message.text !== '/new') {
     return null;
   }
@@ -206,7 +274,7 @@ async function newChatContextHandler(message) {
 }
 
 // 聊天
-async function chatWithOpenAIHandler(message) {
+async function msgChatWithOpenAI(message) {
   try {
     const historyKey = `history:${message.chat.id}`;
     let history = [];
@@ -232,7 +300,7 @@ async function chatWithOpenAIHandler(message) {
   }
 }
 
-// ///////// API
+// /////// --  API
 
 async function sendMessageToChatGPT(message, history) {
   try {
