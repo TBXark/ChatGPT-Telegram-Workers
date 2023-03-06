887d5964b15665ea8f70eaac95c79da45e939e15
TBXark <tbxark@outlook.com>
2023-03-06 22:23:26 +0800

    - 添加默认主页

--- src/router.js
+++ src/router.js
@@ -26,8 +26,67 @@ async function telegramWebhookAction(request) {
   return resp || new Response('NOT HANDLED', {status: 200});
 }
 
+async function defaultIndexAction() {
+  const helpLink = 'https://github.com/TBXark/ChatGPT-Telegram-Workers/blob/master/DEPLOY.md'
+  const issueLink = 'https://github.com/TBXark/ChatGPT-Telegram-Workers/issues'
+  const initLink = './init'
+  const HTML = `
+<html>  
+  <head>
+    <title>ChatGPT-Telegram-Workers</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ChatGPT-Telegram-Workers">
+    <meta name="author" content="TBXark">
+    <style>
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #212529;
+        text-align: left;
+        background-color: #fff;
+      }
+      h1 {
+        margin-top: 0;
+        margin-bottom: 0.5rem;
+      }
+      p {
+        margin-top: 0;
+        margin-bottom: 1rem;
+      }
+      a {
+        color: #007bff;
+        text-decoration: none;
+        background-color: transparent;
+      }
+      a:hover {
+        color: #0056b3;
+        text-decoration: underline;
+      }
+      strong {
+        font-weight: bolder;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>ChatGPT-Telegram-Workers</h1>
+    <p>Deployed Successfully!</p>
+    <p>You must <strong><a href="${initLink}"> >>>>> init <<<<< </a></strong> first.</p>
+    <p>For more information, please visit <a href="${helpLink}">${helpLink}</a></p>
+    <p>If you have any questions, please visit <a href="${issueLink}">${issueLink}</a></p>
+  </body>
+</html>
+  `
+  return new Response(HTML, {status: 200, headers: {'Content-Type': 'text/html'}});
+}
+
 export async function handleRequest(request) {
   const {pathname} = new URL(request.url);
+  if (pathname === `/`) {
+    return defaultIndexAction();
+  }
   if (pathname.startsWith(`/init`)) {
     return bindWebHookAction();
   }
