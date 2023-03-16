68467c0077fd3a789b9a1e71a46484f02de80c95
TBXark <tbxark@outlook.com>
2023-03-16 13:49:12 +0800

    pref: 默认开启GPT3的TOKEN计数模式

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,7 @@ var ENV = {
   // 最大消息长度
   MAX_TOKEN_LENGTH: 2048,
   // 使用GPT3的TOKEN计数
-  GPT3_TOKENS_COUNT: false,
+  GPT3_TOKENS_COUNT: true,
   // 全局默认初始化消息
   SYSTEM_INIT_MESSAGE: "\u4F60\u662F\u4E00\u4E2A\u5F97\u529B\u7684\u52A9\u624B",
   // 全局默认初始化消息角色
@@ -40,9 +40,9 @@ var ENV = {
   // 检查更新的分支
   UPDATE_BRANCH: "master",
   // 当前版本
-  BUILD_TIMESTAMP: 1678944669,
+  BUILD_TIMESTAMP: 1678945730,
   // 当前版本 commit id
-  BUILD_VERSION: "39428bc",
+  BUILD_VERSION: "9e23846",
   // DEBUG 专用
   // 调试模式
   DEBUG_MODE: false,
