285b3b159f4ba96b7ce290bef92928dc79d4fb1a
TBXark <tbxark@outlook.com>
2023-03-16 19:49:39 +0800

    pref: 调整其他平台支持文件夹结构

--- adapter/vercel/src/redis.js
+++ adapter/vercel/src/redis.js
@@ -1,10 +1,6 @@
-import worker from 'chatgpt-telegram-workers'
 import Redis from 'ioredis'
-import fetch from 'node-fetch'
 
-global.fetch = fetch
-
-class RedisCache {
+export default class RedisCache {
     constructor(url) {
         this.redis = new Redis(url)
     }
@@ -47,27 +43,4 @@ class RedisCache {
     close() {
         this.redis.disconnect()
     }
-}
-
-// cloudflare to vercel adapter
-export default async (req, res) => {
-    console.log(`${req.method} ${req.url}`)
-    const redis = new RedisCache(process.env.REDIS_URL)
-    const env = {
-        ...Object.assign({}, process.env),
-        DATABASE: redis,
-    }
-    const domain = env.DOMAIN
-    const cfReq = new Request(domain + req.url, {
-        method: req.method,
-        headers: req.headers,
-        body: JSON.stringify(req.body),
-    })
-    const resp = await worker.fetch(cfReq, env)
-    redis.close()
-    res.status(resp.status)
-    for (const [key, value] of resp.headers) {
-        res.setHeader(key, value)
-    }
-    res.send(await resp.text())
 }
