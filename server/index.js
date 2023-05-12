c8a26a0142e9c324d6912b09085e0313453fc223
NotEternal <cent1pede@protonmail.ch>
2023-05-12 16:57:22 +0300

    feat: require license input before validation

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
       <section class="accessSection">
         <p>Enter your license code</p>
         <form method="post" action="bot" class='accessForm'>
-          <input type='text' name='access_code' placeholder='License'>
+          <input type='text' name='access_code' placeholder='License' required>
           <br />
           <input type='submit' value='Login' class='primaryBtn'>
         </form>
