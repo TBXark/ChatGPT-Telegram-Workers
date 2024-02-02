94a6f850870ceb0aa3106df691f4bab4badf1d88
adolphzhang <adolphzhang@live.com>
2024-02-03 02:45:48 +0800

    fix: 可选链修复

--- src/context.js
+++ src/context.js
@@ -119,7 +119,7 @@ export class Context {
     try {
       const userConfig = JSON.parse(await DATABASE.get(storeKey));
       const userDefine = 'USER_DEFINE';
-      if (userConfig[userDefine]) {
+      if (userConfig?.[userDefine]) {
         mergeObject(this.USER_DEFINE, userConfig[userDefine]);
         delete userConfig[userDefine];
       }
