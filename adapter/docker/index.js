75ad69c3195ac5603447ff2a5ecaabc5a9d15260
adolphzhang <adolphzhang@live.com>
2024-03-04 23:59:18 +0800

    chore: update docker config

--- adapter/docker/index.js
+++ adapter/docker/index.js
@@ -1,9 +1,21 @@
 /* eslint-disable require-jsdoc */
 import adapter from 'cloudflare-worker-adapter';
-import {SqliteCache} from 'cloudflare-worker-adapter/cache/sqlite.js';
-import worker from 'cloudflare-worker-adapter';
+import { RedisCache } from 'cloudflare-worker-adapter/cache/redis.js';
+import fs from 'fs';
 
-const cache = new SqliteCache('./config/cache.sqlite');
+const cache = new RedisCache('redis://localhost:6379');
+
+// 配置版本信息
+try {
+    const buildInfo = JSON.parse(fs.readFileSync('../../dist/buildinfo.json', 'utf-8'));
+    process.env.BUILD_TIMESTAMP = buildInfo.timestamp;
+    process.env.BUILD_VERSION = buildInfo.sha;
+    console.log(buildInfo);
+} catch (e) {
+    console.log(e);
+}
+
+const {default: worker} = await import('chatgpt-telegram-workers');
 
 adapter.startServer(
     8787,
