9b1bcc62c5249135e201a81c3ab5fd130a447830
adolphzhang <adolphzhang@live.com>
2024-03-05 22:38:18 +0800

    chore: modify dockerfile

--- adapter/docker/index.js
+++ adapter/docker/index.js
@@ -1,21 +1,21 @@
 /* eslint-disable require-jsdoc */
 import adapter from 'cloudflare-worker-adapter';
 import { RedisCache } from 'cloudflare-worker-adapter/cache/redis.js';
-import fs from 'fs';
+// import fs from 'fs';
 
 const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 const cache = new RedisCache(redisUrl);
 
 //配置版本信息
-try {
-    const buildInfo = JSON.parse(fs.readFileSync('../../dist/buildinfo.json', 'utf-8'));
-    process.env.BUILD_TIMESTAMP = buildInfo.timestamp;
-    process.env.BUILD_VERSION = buildInfo.sha;
-    console.log(buildInfo);
-} catch (e) {
-    console.log(e);
-}
-console.log(process.env.REDIS_URL);
+// try {
+//     const buildInfo = JSON.parse(fs.readFileSync('../../dist/buildinfo.json', 'utf-8'));
+//     process.env.BUILD_TIMESTAMP = buildInfo.timestamp;
+//     process.env.BUILD_VERSION = buildInfo.sha;
+//     console.log(buildInfo);
+// } catch (e) {
+//     console.log(e);
+// }
+// console.log(process.env.REDIS_URL);
 
 const {default: worker} = await import('chatgpt-telegram-workers');
 // const {default: worker} = await import('../../main.js');
