fa61e766d988776a70c82ba7c81882c1acae524b
tbxark <tbxark@outlook.com>
2024-08-11 00:00:15 +0800

    feat: adapter添加本地部署（无需域名版本）

---
+++ adapter/local_v2/esbuild.config.js
@@ -0,0 +1,13 @@
+import esbuild from 'esbuild';
+
+esbuild.build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    minify: false,
+    outfile: './build/index.js',
+    platform: 'node',
+    format: 'esm',
+    banner: {
+        js: '#!/usr/bin/env node\n/* eslint-disable */',
+    }
+  }).catch(() => process.exit(1));
---
+++ adapter/local_v2/index.js
@@ -0,0 +1,76 @@
+import { ENV, initEnv } from "../../src/config/env.js";
+import { deleteTelegramWebHook, getUpdates } from "../../src/telegram/telegram.js";
+import i18n from "../../src/i18n/index.js";
+import fs from "node:fs";
+import { handleMessage } from "../../src/telegram/message.js";
+import { HttpsProxyAgent } from 'https-proxy-agent';
+import fetch from 'node-fetch';
+
+class MemoryCache {
+    constructor() {
+      this.cache = {};
+    }
+    async get(key) {
+      return this.cache[key];
+    }
+    async put(key, value) {
+      this.cache[key] = value;
+    }
+    async delete(key) {
+      delete this.cache[key];
+    }
+    syncToDisk(path) {
+        fs.writeFileSync(path, JSON.stringify(this.cache));
+    }
+}
+
+// Initialize environment
+const cache = new MemoryCache();
+initEnv({
+    ...(JSON.parse(fs.readFileSync("./config.json"))).vars,
+    DATABASE: cache
+}, i18n);
+
+// Configure https proxy
+const proxy = process.env.https_proxy || process.env.HTTPS_PROXY;
+if (proxy) {
+  console.log(`https proxy: ${proxy}`);
+  const agent = new HttpsProxyAgent(proxy);
+  const proxyFetch = async (url, init) => {
+    return fetch(url, {agent, ...init});
+  };
+  global.fetch = proxyFetch;
+}
+
+
+// Delete all webhooks
+const offset = {};
+for (const token of ENV.TELEGRAM_AVAILABLE_TOKENS) {
+    offset[token] = 0;
+    const [id] = token.split(":");
+    await deleteTelegramWebHook(token);
+    console.log(`Webhook deleted for bot ${id}, If you want to use webhook, please visit  /init`);
+}
+
+// Loop to get updates
+while (true) {
+  for (const token of ENV.TELEGRAM_AVAILABLE_TOKENS) {
+    try {
+      const {result} = await getUpdates(token, offset[token]);
+      if (!result) {
+        continue;
+      }
+      for (const update of result) {
+          if (update.update_id >= offset[token]) {
+              offset[token] = update.update_id + 1;
+          }
+          setImmediate(async () => {
+              await handleMessage(token, update).catch(console.error);
+          });
+      }
+      cache.syncToDisk("./cache.json");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
