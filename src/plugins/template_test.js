7a24a59bd43598c80ed32a23324851106b68c7cc
tbxark <tbxark@outlook.com>
2024-08-22 16:16:58 +0800

    feat: 添加模版测试网页

--- src/plugins/template_test.js
+++ src/plugins/template_test.js
@@ -1,15 +1,15 @@
 import fs from 'node:fs';
 import { executeRequest } from './template.js';
 
-// {
-//     const plugin = './plugins/dns.json';
-//     const template = JSON.parse(fs.readFileSync(plugin, 'utf8'));
-//     executeRequest(template, { DATA: ['B', 'google.com'] }).then(console.log).catch(console.error);
-//     executeRequest(template, { DATA: ['A', 'google.com'] }).then(console.log).catch(console.error);
-// }
-
 {
-    const plugin = './plugins/dicten.json';
+    const plugin = './plugins/dns.json';
     const template = JSON.parse(fs.readFileSync(plugin, 'utf8'));
-    executeRequest(template, { DATA: 'example' }).then(console.log).catch(console.error);
+    // executeRequest(template, { DATA: ['B', 'google.com'] }).then(console.log).catch(console.error);
+    executeRequest(template, { DATA: ['A', 'www.bilibili.com'] }).then(r => console.log(r.content)).catch(console.error);
 }
+
+// {
+//     const plugin = './plugins/dicten.json';
+//     const template = JSON.parse(fs.readFileSync(plugin, 'utf8'));
+//     executeRequest(template, { DATA: 'example' }).then(console.log).catch(console.error);
+// }
