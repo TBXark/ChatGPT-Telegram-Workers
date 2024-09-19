4e59a3b9875e91d1b22ff255d8ff6c1e7ab1f7d9
tbxark <tbxark@outlook.com>
2024-09-19 11:06:58 +0800

    chore: 升级依赖

--- src/config/env.test.ts
+++ src/config/env.test.ts
@@ -3,11 +3,11 @@ import { parse } from 'toml';
 import { ENV } from './env';
 
 {
-    const toml = readFileSync('../../wrangler.toml', 'utf8');
+    const toml = readFileSync('./wrangler.toml', 'utf8');
     const config = parse(toml);
     ENV.merge({
         ...config.vars,
         DATABASE: {},
     });
-    // console.log(JSON.stringify(ENV, null, 2));
+    console.log(JSON.stringify(ENV, null, 2));
 }
