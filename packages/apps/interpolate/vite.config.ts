5ee0d973c2ba0da1ce7d621b1294641a34257951
tbxark <tbxark@outlook.com>
2024-11-15 09:57:25 +0800

    chore: 拆分插件测试网页css

--- packages/apps/interpolate/vite.config.ts
+++ packages/apps/interpolate/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         emptyOutDir: true,
+        minify: false,
         rollupOptions: {
             input: 'index.html',
             output: {
