3d5f1e7b4b199d7417e5c4acf74e75247d4d3271
Tom <yusencao@outlook.com>
2024-11-28 15:52:57 +0800

    fix: 模型选择页面缺项

--- packages/lib/core/src/telegram/callback_query/system.ts
+++ packages/lib/core/src/telegram/callback_query/system.ts
@@ -71,6 +71,10 @@ export class ModelListCallbackQueryHandler implements CallbackQueryHandler {
 
         let currentRow: Telegram.InlineKeyboardButton[] = [];
         for (let i = page * maxRow * maxCol; i < models.length; i++) {
+            currentRow.push({
+                text: models[i],
+                callback_data: `cm:${JSON.stringify([agent, models[i]])}`,
+            });
             if (i % maxCol === 0) {
                 keyboard.push(currentRow);
                 currentRow = [];
@@ -78,10 +82,6 @@ export class ModelListCallbackQueryHandler implements CallbackQueryHandler {
             if (keyboard.length >= maxRow) {
                 break;
             }
-            currentRow.push({
-                text: models[i],
-                callback_data: `cm:${JSON.stringify([agent, models[i]])}`,
-            });
         }
         if (currentRow.length > 0) {
             keyboard.push(currentRow);
