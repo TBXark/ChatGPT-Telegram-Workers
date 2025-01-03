a3bd5ee85cc441e32c49c9f167ac7ed00eb0db0e
tbxark <tbxark@outlook.com>
2025-01-03 15:19:10 +0800

    perf: 优化本地telegram polling

--- packages/apps/local/src/index.ts
+++ packages/apps/local/src/index.ts
@@ -1,15 +1,9 @@
-import type { TelegramBotAPI } from '@chatgpt-telegram-workers/core';
-import type { GetUpdatesResponse } from 'telegram-bot-api-types';
 import * as fs from 'node:fs';
-import { CHAT_AGENTS, createRouter, createTelegramBotAPI, ENV, handleUpdate } from '@chatgpt-telegram-workers/core';
+import * as process from "node:process";
+import { CHAT_AGENTS, createRouter, ENV, handleUpdate } from '@chatgpt-telegram-workers/core';
 import { injectNextChatAgent } from '@chatgpt-telegram-workers/next';
 import { createCache, defaultRequestBuilder, initEnv, installFetchProxy, startServerV2 } from 'cloudflare-worker-adapter';
-
-const {
-    CONFIG_PATH = '/app/config.json',
-    TOML_PATH = '/app/wrangler.toml',
-    NEXT_ENABLE = '0',
-} = process.env;
+import { runPolling } from './telegram';
 
 interface Config {
     database: {
@@ -25,68 +19,29 @@ interface Config {
     mode: 'webhook' | 'polling';
 }
 
+const {
+    CONFIG_PATH = '/app/config.json',
+    TOML_PATH = '/app/wrangler.toml',
+    NEXT_ENABLE = '0',
+} = process.env;
+
 // 读取配置文件
 const config: Config = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'));
 
-if (config.proxy) {
-    installFetchProxy(config.proxy);
-}
-
 // 初始化数据库
-const cache = createCache(config?.database?.type, {
-    uri: config.database.path || '',
-});
+const cache = createCache(config?.database?.type, { uri: config.database.path || '' });
 console.log(`database: ${config?.database?.type} is ready`);
 
 // 初始化环境变量
 const env = initEnv(TOML_PATH, { DATABASE: cache });
 ENV.merge(env);
 
-// long polling 模式
-async function runPolling() {
-    const clients: Record<string, TelegramBotAPI> = {};
-    const offset: Record<string, number> = {};
-    for (const token of ENV.TELEGRAM_AVAILABLE_TOKENS) {
-        offset[token] = 0;
-        const api = createTelegramBotAPI(token);
-        clients[token] = api;
-        const name = await api.getMeWithReturns();
-        await api.deleteWebhook({});
-        console.log(`@${name.result.username} Webhook deleted, If you want to use webhook, please set it up again.`);
-    }
-
-    const keepRunning = true;
-    // eslint-disable-next-line no-unmodified-loop-condition
-    while (keepRunning) {
-        for (const token of ENV.TELEGRAM_AVAILABLE_TOKENS) {
-            try {
-                const resp = await clients[token].getUpdates({ offset: offset[token] });
-                if (resp.status === 429) {
-                    const retryAfter = Number.parseInt(resp.headers.get('Retry-After') || '');
-                    if (retryAfter) {
-                        console.log(`Rate limited, retry after ${retryAfter} seconds`);
-                        await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
-                        continue;
-                    }
-                }
-                const { result } = await resp.json() as GetUpdatesResponse;
-                for (const update of result) {
-                    if (update.update_id >= offset[token]) {
-                        offset[token] = update.update_id + 1;
-                    }
-                    setImmediate(async () => {
-                        await handleUpdate(token, update).catch(console.error);
-                    });
-                }
-            } catch (e) {
-                console.error(e);
-            }
-        }
-    }
-}
-
+// 注入 Next.js Chat Agent
 if (NEXT_ENABLE !== '0') {
     injectNextChatAgent(CHAT_AGENTS);
+}
+if (config.proxy) {
+    installFetchProxy(config.proxy);
 }
 
 // 启动服务
@@ -101,5 +56,8 @@ if (config.mode === 'webhook' && config.server !== undefined) {
         router.fetch,
     );
 } else {
-    runPolling().catch(console.error);
+    runPolling(
+        ENV.TELEGRAM_AVAILABLE_TOKENS,
+        handleUpdate,
+    ).catch(console.error);
 }
