286e46f61ec5193719e02b8019a2a22a856a87b6
tbxark <tbxark@outlook.com>
2025-01-16 21:20:52 +0800

    chore: 修改workers-next和workers-mk2编译生产dist逻辑

--- packages/apps/workers-mk2/vite.config.ts
+++ packages/apps/workers-mk2/vite.config.ts
@@ -2,6 +2,4 @@ import { createShareConfig } from '../../../vite.config.shared';
 
 export default createShareConfig({
     root: __dirname,
-    nodeExternals: true,
-    excludeMonoRepoPackages: true,
 });
